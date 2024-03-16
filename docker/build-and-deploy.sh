591801a996844a5f9b0555606a85fed9c9cebf39
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-03-16 10:32:22 +0530

    Update docker

---
+++ docker/build-and-deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$1" == "traefik" ]; then
+    DOCKER_COMPOSE_COMMAND="docker-compose -f docker-compose.yml -f docker-compose.traefik.yml up -d"
+else
+    DOCKER_COMPOSE_COMMAND="docker-compose -f docker-compose.yml up -d"
+fi
+
+if [ -f .env ]; then
+    source .env
+else
+    echo ".env file not found!"
+    exit 1
+fi
+
+if [ "$GIT" != "false" ]; then
+    rm -rf ./src
+    git clone "$GIT_URL" src
+else
+    if [ ! -d "./src" ]; then
+        echo "src dir not found!"
+        exit 1
+    fi
+fi
+
+docker-compose down
+
+if [ "$(docker images -q "${ALIAS}-image" 2>/dev/null)" ]; then
+    docker rmi "${ALIAS}-image"
+fi
+
+DOCKER_BUILDKIT=1 docker build -t "${ALIAS}-image" .
+
+eval "$DOCKER_COMPOSE_COMMAND"
+
+docker system prune --all
